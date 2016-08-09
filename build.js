904897613b37e42f8fcaad8a489a0b76ef937909
Thomas Wade <w1220w@126.com>
2016-08-09 18:16:31 +0800

    kkk

--- build.js
+++ build.js
@@ -18,7 +18,6 @@
     findNestedDependencies: true,
     
     paths: {
-        
         'logOut':'../../login/logOut',
         'getData':'../../getData/getData',
         'head'    : '../../head/head',
@@ -26,94 +25,69 @@
         'poster'  : '../../poster/poster',
         'common'  : '../../common/common',
         'details' : '../../details/details',
-     
         'comment' : '../../comment/comment',
         'explore':'../../explore/explore',
         'tile':'../../explore/tile',
         'like':'../../like/like',
         'pagination':'../../pagination/pagination',
         'backButton':'../../backButton/backButton'
-   
     },
-    
     shim: {
-       
         backButton:{
             deps:[
-               
                 'explore',
                 'details',
                 'poster'
             ]
         },
-       
         like:{
             deps:[
-               
                 'getData',
                 'poster',
-            
             ]
         },
-
         head: {
             deps: [
-               
                 'comment',
-               
             ]
         },
         poster:{
             deps:[
-              
                 'head',
-                
                 'common',
                 'logOut',
-              
             ]
         },
         pagination:{
             deps:[
                 'poster',
                 "getData",
-             
             ]
         },
         sideBar: {
             deps: [
-               
                 'getData',
                 'head',
                 'common',
                 'logOut',
                 'poster',
-              
                 'explore'
-                
             ]
         },
         explore:{
             deps:[
-               
                 'getData',
                 'common',
                 'tile',
             ]
         },
-       
         details:{
             deps:[
-               
                 "head",
                 "poster",
-             
                 'comment',
                 'common'
             ]
         }
-    
     }
-    
-
 })
